33101225b61f9c3e97cae4806485f47996422308
Indiana <atwoodi26@up.edu>
2024-03-26 10:34:26 -0700

    Ran through game - Indiana 3/26

--- app/src/main/java/edu/up/cs301/Stratego/StrategoHumanPlayer.java
+++ app/src/main/java/edu/up/cs301/Stratego/StrategoHumanPlayer.java
@@ -92,8 +92,107 @@ public class StrategoHumanPlayer extends GameHumanPlayer implements OnClickListe
 		game.sendAction(move1);
 		testResultsTextView.append("\nAttack red 9 (5,1) with blue 2 (2,1)");
 
-		//MovePiece move2 = new MovePiece(this,2,1,5,1);
+		MovePiece move2 = new MovePiece(this,5,0,4,0);
+		game.sendAction(move2);
+		testResultsTextView.append("\nMove red 10 (5,0) to (5,1)");
 
+		MovePiece move3 = new MovePiece(this,2,9,3,9);
+		game.sendAction(move3);
+		testResultsTextView.append("\nMove blue 10 (2,9) to (3,9)");
+
+		MovePiece move4 = new MovePiece(this,5,8,6,8);
+		game.sendAction(move4);
+		testResultsTextView.append("\nMove red 6 (5,8) to empty (6,8)");
+
+		MovePiece move5 = new MovePiece(this,3,9,4,9);
+		game.sendAction(move5);
+		testResultsTextView.append("\nMove blue 10 (3,9) to (4,9)");
+
+		MovePiece move6 = new MovePiece(this,4,8,4,9);
+		game.sendAction(move6);
+		testResultsTextView.append("\nMove red 6 (4,8) to blue 10 (4,9)");
+
+		MovePiece move7 = new MovePiece(this,4,9,5,9);
+		game.sendAction(move7);
+		testResultsTextView.append("\nMove blue 10 (4,9) to red 5 (5,9)");
+
+		MovePiece move8 = new MovePiece(this,6,8,2,8);
+		game.sendAction(move8);
+		testResultsTextView.append("\nMove red 2 (6,8) to blue 11 (2,8)");
+
+		MovePiece move9 = new MovePiece(this,1,1,2,1);
+		game.sendAction(move9);
+		testResultsTextView.append("\nMove blue 5 (1,1) to empty (2,1)");
+
+		MovePiece move10 = new MovePiece(this,6,7,6,8);
+		game.sendAction(move10);
+		testResultsTextView.append("\nMove red 3 (6,7) to empty (6,8)");
+
+		MovePiece move11 = new MovePiece(this,2,4,3,4);
+		game.sendAction(move11);
+		testResultsTextView.append("\nMove blue 1 (2,4) to empty (3,4)");
+
+		MovePiece move12 = new MovePiece(this,6,8,7,8);
+		game.sendAction(move12);
+		testResultsTextView.append("\nMove red 3 (6,8) to empty (7,8)");
+
+		MovePiece move13 = new MovePiece(this,2,1,3,1);
+		game.sendAction(move13);
+		testResultsTextView.append("\nMove blue 5 (2,1) to empty (3,1)");
+
+		MovePiece move14 = new MovePiece(this,5,8,4,8);
+		game.sendAction(move14);
+		testResultsTextView.append("\nMove red 3 (5,8) to empty (4,8)");
+
+		MovePiece move15 = new MovePiece(this,1,9,4,9);
+		game.sendAction(move15);
+		testResultsTextView.append("\nMove blue 2 (1,9) to empty (4,9)");
+
+		MovePiece move16 = new MovePiece(this,4,8,3,8);
+		game.sendAction(move16);
+		testResultsTextView.append("\nMove red 3 (4,8) to empty (3,8)");
+
+		MovePiece move17 = new MovePiece(this,2,0,4,0);
+		game.sendAction(move17);
+		testResultsTextView.append("\nMove blue 2 (2,0) to red 10 (4,0)");
+
+		MovePiece move18 = new MovePiece(this,3,8,2,8);
+		game.sendAction(move18);
+		testResultsTextView.append("\nMove red 3 (3,8) to blue 11 (2,8)");
+
+		MovePiece move19 = new MovePiece(this,5,9,6,9);
+		game.sendAction(move19);
+		testResultsTextView.append("\nMove blue 10 (5,9) to red 2 (6,9)");
+
+		MovePiece move20 = new MovePiece(this,5,7,4,7);
+		game.sendAction(move20);
+		testResultsTextView.append("\nMove red 6 (5,7) to lake (4,7) INVALID, RED AGAIN");
+
+		MovePiece move21 = new MovePiece(this,5,7,6,7);
+		game.sendAction(move21);
+		testResultsTextView.append("\nMove red 6 (5,7) to empty (6,7)");
+
+		MovePiece move22 = new MovePiece(this,6,9,6,8);
+		game.sendAction(move22);
+		testResultsTextView.append("\nMove blue 10 (6,9) to empty (6,8)");
+
+		MovePiece move23 = new MovePiece(this,5,1,4,1);
+		game.sendAction(move23);
+		testResultsTextView.append("\nMove red 9 (5,1) to empty (4,1)");
+
+		MovePiece move24 = new MovePiece(this,6,8,7,8);
+		game.sendAction(move24);
+		testResultsTextView.append("\nMove blue 10 (6,8) to red 11 (7,8)");
+
+		MovePiece move25 = new MovePiece(this,4,1,3,1);
+		game.sendAction(move25);
+		testResultsTextView.append("\nMove red 9 (4,1) to blue 5 (3,1)");
+
+		MovePiece move26 = new MovePiece(this,4,9,7,9);
+		game.sendAction(move26);
+		testResultsTextView.append("\nMove blue 2 (4,9) to red 0 (7,9), CAPTURED FLAG");
+
+		testResultsTextView.append("\nBLUE WINS");
 
 		// creation of the secondInstance and the deep copy
 		StrategoState secondInstance = new StrategoState();
@@ -103,6 +202,8 @@ public class StrategoHumanPlayer extends GameHumanPlayer implements OnClickListe
 		// implements toString() for both copies
 		String fstCpy = firstCopy.toString();
 		String secCpy = secondCopy.toString();
+
+		String gme = game.toString();
 
 		// if the copies are identical, we append the strings to the TextView
 		if(fstCpy.equals(secCpy)) {
@@ -115,6 +216,8 @@ public class StrategoHumanPlayer extends GameHumanPlayer implements OnClickListe
 		else {
 			testResultsTextView.append("Copies are not identical... :(");
 		}
+
+		testResultsTextView.append("\n\n" + gme);
 
 	}// onClick
 	
